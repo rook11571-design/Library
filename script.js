7efc1dc2f253927365d47942368b1fa80db49709
rook <rook11571@gmail.com>
2026-01-07 05:03:47 +0800

    fixes a few bug and clarifies some important concepts of function and objects

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 const container = document.querySelector("#container");
 function Book(title, author, pages, read) {
   this.title = title;
@@ -6,27 +6,19 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.id = crypto.randomUUID();
-  this.info = () => {
-    return `${title} by ${author}, ${pages} pages, ${
-      read ? "read" : "not read yet"
-    }`;
-  };
-  this.toggle = () => {
-    if (read) {
-      read = false;
-    } else {
-      read = true;
-    }
-    this.read = read;
-  };
   addBookToLibrary(this);
 }
+
+Book.prototype.toggle = function() {
+  //do not use arrow function as arrow function do not have their own this!!!!
+  this.read = !this.read;
+};
 
 function addBookToLibrary(Book) {
   myLibrary.push(Book);
 }
 
-function displayLibrary(myLibrary) {
+function displayLibrary() {
   myLibrary.forEach((book) => {
     console.log(book);
     const card = document.createElement("div");
@@ -43,15 +35,16 @@ function displayLibrary(myLibrary) {
     bookPages.textContent = `Pages    : ${book.pages}`;
 
     const Status = document.createElement("div");
-    Status.id = "status";
+    Status.className = "status";
     const bookStatus = document.createElement("div");
     bookStatus.textContent = `Status  : ${book.read ? "Read" : "Not Read"}`;
     const readBtn = document.createElement("button");
     readBtn.textContent = "change status";
     readBtn.addEventListener("click", () => {
       book.toggle();
+      console.log(book)
       bookStatus.textContent = `Status  : ${book.read ? "Read" : "Not Read"}`;
-      console.log(book.info());
+      
     });
 
     const bookId = document.createElement("div");
@@ -60,8 +53,14 @@ function displayLibrary(myLibrary) {
     const removeBtn = document.createElement("button");
     removeBtn.textContent = "Remove";
     removeBtn.addEventListener("click", (e) => {
-      myLibrary = myLibrary.filter((element) => element.id !== book.id);
-      e.target.parentElement.remove();
+      // const index = myLibrary.indexOf(book);//this is the correct way of removing object from an array out of scope
+      // if(index>-1){
+      //   myLibrary.splice(index, 1);
+      // }
+      myLibrary = myLibrary.filter((element) => element.id !== book.id);//this is also a way to remove object from an array if the myLibrary variable is not passed to the function
+      //e.target.parentElement.remove();//it is not the best way to do this as the UI and data might be out of sync
+      removeLibrary();// this clear the UI
+      displayLibrary();// this reset the UI and ensure UI and data are in sync
     });
 
     Status.appendChild(bookStatus);
@@ -94,21 +93,23 @@ const submitBtn = document.querySelector("#submit");
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const form = document.querySelector("form");
-  
+  const checkboxElement =document.getElementById('read');
+  const isChecked = checkboxElement.checked
   const newBook = new Book(
     form.title.value,
     form.author.value,
     form.page.value,
-    form.read.value
+    isChecked
   );
 
   removeLibrary();
-  displayLibrary(myLibrary);
+  displayLibrary();
   form.reset();
   const dialog = document.querySelector("dialog");
+  console.log(newBook)
   dialog.close();
 });
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 const abc = new Book("abc", "John", 3, true);
-displayLibrary(myLibrary);
+displayLibrary();
